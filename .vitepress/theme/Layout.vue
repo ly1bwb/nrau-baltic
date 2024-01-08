3b6f89808f162e4c9952aca30bb09db37463bbd3
domnantas <hello@domnantas.lt>
2024-01-08 13:08:10 +0200

    Add page title under homepage anchor

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -22,14 +22,17 @@ const navigationItems = {
 		<nav
 			class="flex justify-between items-center max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0"
 		>
-			<a href="/" class="mr-4">
+			<a
+				href="/"
+				class="mr-4 flex items-center gap-4 font-semibold text-lg text-gray-700 dark:text-gray-200"
+			>
 				<img
 					class="w-10 h-[78px]"
 					src="/logo.svg"
-					alt="NRAU-Baltic contest logo"
+					alt="NRAU Baltic contest logo"
 				/>
+				NRAU-Baltic contest
 			</a>
-			<div class="w-full custom-block-title text-gray-700 dark:text-gray-200">NRAU-Baltic contest</div>
 			<Navigation class="hidden lg:flex" :navigation-items="navigationItems" />
 			<MobileNavigation class="lg:hidden" :navigation-items="navigationItems" />
 		</nav>
