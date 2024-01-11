4d89ea013253a4459f54ee083a9385cd4387af8e
domnantas <hello@domnantas.lt>
2024-01-11 12:46:52 +0200

    Enable hydration debugging

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -175,5 +175,9 @@ export default defineConfigWithTheme<ThemeConfig>({
 		ssr: {
 			noExternal: ["echarts", "zrender", "vue-echarts", "resize-detector"],
 		},
+		define: {
+			// enable hydration mismatch details in production build
+			__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "true",
+		},
 	},
 });
